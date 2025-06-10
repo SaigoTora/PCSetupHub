b42dd8d70302b3e7fe309b4c48a6877cf56b128a
Bohdan Kalnytskyi <bogdan201375@gmail.com>
2025-06-10 17:45:26 +0300

    Create initial controllers for PC components.

---
+++ PCSetupHub.Web/Controllers/HardwareComponents/SsdController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PCSetupHub.Web.Controllers.HardwareComponents
+{
+	public class SsdController : Controller
+	{
+		public IActionResult Add()
+		{
+			return Ok($"Add from {GetType().Name}");
+		}
+		public IActionResult Select()
+		{
+			return Ok($"Select from {GetType().Name}");
+		}
+		public IActionResult Clear()
+		{
+			return Ok($"Clear from {GetType().Name}");
+		}
+		public IActionResult Edit()
+		{
+			return Ok($"Edit from {GetType().Name}");
+		}
+		public IActionResult Delete()
+		{
+			return Ok($"Delete from {GetType().Name}");
+		}
+	}
+}
---
+++ PCSetupHub.Web/Controllers/HardwareComponents/VideoCardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PCSetupHub.Web.Controllers.HardwareComponents
+{
+	public class VideoCardController : Controller
+	{
+		public IActionResult Select()
+		{
+			return Ok($"Select from {GetType().Name}");
+		}
+		public IActionResult Clear()
+		{
+			return Ok($"Clear from {GetType().Name}");
+		}
+		public IActionResult Edit()
+		{
+			return Ok($"Edit from {GetType().Name}");
+		}
+		public IActionResult Delete()
+		{
+			return Ok($"Delete from {GetType().Name}");
+		}
+	}
+}
